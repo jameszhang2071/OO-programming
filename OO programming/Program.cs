95c5ac03d0973d638da235494452ff0de169e94d
James Zhang <jameszhang2071@gmail.com>
2023-11-29 21:45:40 +1100

    Add project files.

---
+++ OO programming/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OO_programming
+{
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+
+    /// <summary>
+    /// Class a capture details accociated with an employee's pay slip record
+    /// </summary>
+    public class PaySlip
+    {
+    }
+
+    /// <summary>
+    /// Base class to hold all Pay calculation functions
+    /// Default class behaviour is tax calculated with tax threshold applied
+    /// </summary>
+    public class PayCalculator
+    {
+     
+    }
+
+    /// <summary>
+    /// Extends PayCalculator class handling No tax threshold
+    /// </summary>
+    public class PayCalculatorNoThreshold : PayCalculator
+    {
+     
+    }
+
+    /// <summary>
+    /// Extends PayCalculator class handling With tax threshold
+    /// </summary>
+    public class PayCalculatorWithThreshold : PayCalculator
+    {
+     
+    }
+}
